0346addf4102060352e2a3a65c861da20492f1af
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-04-11 11:35:59 +0200

    Part08  Product Modules - Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	public class Product : BaseEntity
+	{
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public int BrandId { get; set; } // FK => ProductBrand
+        public ProductBrand Brand { get; set; } // Navigational Propert [One]
+
+        public int CategoryId { get; set; } // FK => ProductCategory
+        public ProductCategory Category { get; set; } // Navigational Propert [One]
+
+	}
+}
