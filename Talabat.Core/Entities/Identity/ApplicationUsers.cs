f1dee63df39efe490ce8a44396b57386f09ad67e
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-04-29 20:33:38 +0300

    Part 01 Identity - Create Database

---
+++ Talabat.Core/Entities/Identity/ApplicationUsers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Talabat.Core.Entities.Identity
+{
+    public class ApplicationUsers : IdentityUser
+    {
+        public string DisplayName { get; set; } = null!;
+
+        public Address? Address { get; set; } = null; // Navigational Property [One]
+
+    }
+}
---
+++ Talabat.Infrastructure/_Identity/ApplicationIdentityDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.Infrastructure._Identity
+{
+	public class ApplicationIdentityDbContext : IdentityDbContext<ApplicationUsers>
+	{
+		public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options)
+			: base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<ApplicationUsers>()
+				.HasOne(U => U.Address)
+				.WithOne(A => A.User)
+				.HasForeignKey<Address>(A => A.ApplicationUserId);
+		}
+
+	}
+}
