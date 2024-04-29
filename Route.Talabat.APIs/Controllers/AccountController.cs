04910a0c3458a5d5a1b6bd32dbff52108932df42
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-04-29 23:16:06 +0300

    Part 03 Login Endpoint

---
+++ Route.Talabat.APIs/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Route.Talabat.APIs.DTO.IdentityDto;
+using Route.Talabat.APIs.Errors;
+using Talabat.Core.Entities.Identity;
+
+namespace Route.Talabat.APIs.Controllers
+{
+	public class AccountController : BaseApiController
+	{
+		private readonly UserManager<ApplicationUsers> _userManager;
+		private readonly SignInManager<ApplicationUsers> _signInManager;
+
+		public AccountController(
+			UserManager<ApplicationUsers> userManager,
+			SignInManager<ApplicationUsers> signInManager) 
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		[HttpPost("login")] // POST : /api/Account/login
+		public async Task<ActionResult<UserDto>> Login(LoginDto model)
+		{
+			var user = await _userManager.FindByEmailAsync(model.Email);
+
+			if (user is null) return Unauthorized(new ApiResponse(401 , "Invalid Login"));
+
+			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+			if (result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+
+			return Ok(new UserDto()
+			{
+				DisplayName = user.DisplayName,
+				Email = user.Email,
+				Token = "This will be token"
+			});
+		}
+	}
+}
